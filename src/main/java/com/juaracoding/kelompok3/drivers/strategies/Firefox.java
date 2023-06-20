3cfcf2a27e5378e72157d9ee8ed3019b3ee647d9
Lita Nurlaelati <litanurlaelati@gmail.com>
2023-06-20 23:15:57 +0700

    Fix typo on driver firefox

--- src/main/java/com/juaracoding/kelompok3/drivers/strategies/Firefox.java
+++ src/main/java/com/juaracoding/kelompok3/drivers/strategies/Firefox.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class Firefox implements DriverStrategy {
     @Override
     public WebDriver setStrategy() {
-        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/geckodriver");
+        System.setProperty("webdriver.firefox.driver", "/usr/local/bin/geckodriver");
         WebDriver driver = new FirefoxDriver();
         return driver;
     }
